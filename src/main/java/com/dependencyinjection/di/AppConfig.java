fce549aa37381cddef84ade216ac86550916276c
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-04-19 09:07:13 -0400

    Add new repository to read files from json

--- src/main/java/com/dependencyinjection/di/AppConfig.java
+++ src/main/java/com/dependencyinjection/di/AppConfig.java
@@ -1,10 +1,19 @@
 package com.dependencyinjection.di;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
+
+import com.dependencyinjection.di.repositories.ProductRepositoryJson;
 
 @Configuration
 @PropertySource("classpath:config.properties")
 public class AppConfig {
   
+  @Bean
+  @Primary
+  ProductRepositoryJson productRepositoryJson() {
+    return new ProductRepositoryJson();
+  }
 }
